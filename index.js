4fd2d86ca816114b66a0ea3bb8350e0de203413d
Adriano Marino <adriano.marino1992@gmail.com>
2020-04-08 14:12:44 -0300

    nodejs correção

--- index.js
+++ index.js
@@ -3,10 +3,6 @@ var express = require('express');
 var app = express();
 
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/Index.html');
-})
-
 //CRIANDO DIRETORIO ACESSIVEL !!!!
 app.use(express.static(__dirname + '/App'));
 
