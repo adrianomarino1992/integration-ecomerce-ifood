fbca4b458361f125a37772c6b21eebcbf6d2d0a3
Adriano Marino <adriano.marino1992@gmail.com>
2020-04-08 16:28:03 -0300

    nodejs correção #8

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ var app = express();
 //var port = 3000;
 var port = process.env.PORT;
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/App/Index.html');
-})
+// app.get('/', (req, res) => {
+//     res.sendFile(__dirname + '/App/Index.html');
+// })
+
+
+app.use(express.static(__dirname + '/App'));
 
 app.listen(port, () => {
     console.log('servidor rodando em ' + port);
