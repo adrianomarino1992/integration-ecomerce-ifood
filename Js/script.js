2a98963a02f3636d4cef66307f922dbf0f3e2953
Adriano Marino <adriano.marino1992@gmail.com>
2020-04-08 12:43:21 -0300

    release layout mobile * scripts

--- Js/script.js
+++ Js/script.js
@@ -1,1 +1,5 @@
-// window.location = "https://api.whatsapp.com/send?phone=+5512982068255&text=Hi, I contacted you Through your website.";
+$('.btn-quero').on('click', () => {
+
+    window.open("https://api.whatsapp.com/send?phone=+5512982068255&text=Olá B2B, gostaria de utilizar delivery/ifood no meu sistema ! ");
+
+})
