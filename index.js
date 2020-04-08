a02d7e5fd3bc082c5044ac615f9d69d074a16b33
Adriano Marino <adriano.marino1992@gmail.com>
2020-04-08 15:28:35 -0300

    nodejs correção #5

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 var http = require('http');
 var express = require('express');
 var app = express();
-
+//var port = 3000;
+var port = process.env.PORT;
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/App/Index.html');
 })
 
-// app.get('/', (req, res) => {
-//     res.sendFile(__dirname + '/App/Index.html');
-// })
-
-
-app.listen(process.env.PORT, () => {
-    console.log('servidor rodando');
+app.listen(port, () => {
+    console.log('servidor rodando em ' + port);
 })
