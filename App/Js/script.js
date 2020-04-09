05142d2e96be79df67e0403aebe2c3f402e6250a
Adriano Marino <adriano.marino1992@gmail.com>
2020-04-09 16:10:36 -0300

    scrool iphone x #1

--- App/Js/script.js
+++ App/Js/script.js
@@ -1,25 +1,13 @@
 (() => {
 
-    // $(document).ready(function() {
-    //     if (navigator.userAgent.indexOf('iPhone') != -1) {
-
-    //         $('body').css('MozTransform', 'scale(0.9)');
-
-    //         $('body').css('zoom', '90%');
-
-    //     }
-    // });
-
-
-
     $('.btn-quero').on('click', (event) => {
 
         let button = event.currentTarget;
 
         if ($(button).text().indexOf("IFOOD") != -1) {
-            window.open("https://api.whatsapp.com/send?phone=+5512982068255&text=Olá B2BTi, gostaria de mais informações sobre o iFood!");
+            window.open("https://api.whatsapp.com/send?phone=+5512981417816&text=Olá B2BTi, gostaria de mais informações sobre o iFood!");
         } else {
-            window.open("https://api.whatsapp.com/send?phone=+5512982068255&text=Olá B2BTi, gostaria de mais informações sobre o Delivery!");
+            window.open("https://api.whatsapp.com/send?phone=+5512981417816&text=Olá B2BTi, gostaria de mais informações sobre o Delivery!");
         }
 
     });
@@ -28,4 +16,5 @@
     $('.redirect-to-site').on('click', () => {
         window.open("https://www.b2bti.com.br/");
     })
+
 })();
